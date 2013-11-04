412eb46ecdb63665f443edcc346f82949288e170
Stephanie Williams (sadamson) <sadamson@email.unc.edu>
2013-11-04 16:00:01 -0500

    Adding index page back to repo

---
+++ index.php
@@ -0,0 +1,35 @@
+<?php
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+?>
+<html>
+    <head>
+        <title>NCDHC DPLA/OAI Aggregation Tools</title>
+        <link rel="stylesheet" type="text/css" href="style.css"/>
+    </head>
+</html>
+
+<div id="container">
+    <h1>NCDHC DPLA/OAI Aggregation Tools</h1>
+    <ul>
+        <li>
+            <a href="mapping_checker"><h2>DC Mapping Checker</h2>
+            <p>Check incoming oai_dc feeds for correct Dublin Core mappings.</p></a>
+        </li>
+        <li>
+            <a href="facet_viewer/"><h2>DC Facet Viewer</h2>
+            <p>See the contents of Dublin Core fields in an incoming OAI feed according to frequency.</p></a>
+        </li>                                
+        <li>
+            <a href="reqdata_checker/"><h2>Required Data Checker</h2>
+            <p>Check incoming oai_dc or outgoing MODS for required data.</p></a>
+        </li>
+        <li>
+            <a href="oai_browser/"><h2>DigitalNC OAI URL Reference</h2>
+            <p>Quick OAI URL construction for DigitalNC feed.</p></a>
+        </li>
+    </ul>
+</div>
+
